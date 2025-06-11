5871d3be399fe0a0d3852087fe3231a1443fb8d9
Alexis-Vargas-Xav <alexisyasacama@gmai.com>
2025-06-10 23:33:04 -0500

    Primera prueba

---
+++ src/main/java/ec/edu/espoch/pruebapararamas/PruebaParaRamas.java
@@ -0,0 +1,12 @@
+
+package ec.edu.espoch.pruebapararamas;
+
+
+public class PruebaParaRamas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //Este programa esta hecho con el fin de practicar la subida de projectos al git de forma colaborativa
+    }
+}
